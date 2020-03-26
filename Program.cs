ee7c99ecd029cdbfce8a3ca20ae642e88181d68a
Chad Luangrath <chad.luangrath@ilosvideos.com>
2020-03-26 12:15:08 -0500

    move a bunch of files around into their correct places

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using CommandLine;
+using System;
+using System.IO;
+
+namespace obs_cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                       if (o.Verbose)
+                       {
+                           Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                       }
+                       else
+                       {
+                           Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example!");
+                       }
+
+                       if (!string.IsNullOrWhiteSpace(o.Write))
+                       {
+                           var pathToUse = string.IsNullOrWhiteSpace(o.WritePath) ? @"C:\Users\Chad\Documents\test\test.txt" : o.WritePath;
+                           using (StreamWriter file = new StreamWriter(pathToUse, true))
+                           {
+                               file.WriteLine(o.Write);
+                           }
+                       }
+                   });
+
+            Console.ReadLine();
+        }
+    }
+}
