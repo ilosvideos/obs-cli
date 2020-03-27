1dad4c93fa8c61ac5f81eed9bb4a2f8902179c31
Chad Luangrath <chad.luangrath@ilosvideos.com>
2020-03-27 15:18:23 -0500

    start initializing OBS

--- Commands/Implementations/Initialize.cs
+++ Commands/Implementations/Initialize.cs
@@ -1,4 +1,7 @@
-﻿using obs_cli.Helpers;
+﻿using OBS;
+using obs_cli.Helpers;
+using obs_cli.Utility;
+using System;
 
 namespace obs_cli.Commands.Implementations
 {
@@ -14,8 +17,131 @@ namespace obs_cli.Commands.Implementations
 
         public void Execute()
         {
-            FileWriteService.WriteToFile("in initialize execute");
-            FileWriteService.WriteToFile("this is after the execute?");
+            FileWriteService.WriteToFile("initialize command start");
+
+            if (!Obs.Startup("en-US"))
+            {
+                // todo: if any exceptions are thrown in this app, we need to bubble it all up to a single terminate code so consuming apps know that it shut down
+                throw new ApplicationException("Startup failed.");
+            }
+
+            ResetAudioInfo();
+
+            FileWriteService.WriteToFile("ResetAudioInfo successful");
+
+            // todo: ResetVideoInfo();
+
+            Obs.LoadAllModules();
+
+            FileWriteService.WriteToFile("Obs.LoadAllModules successful");
+
+            FileWriteService.WriteToFile("initialize command end");
         }
+
+        private void ResetAudioInfo()
+        {
+            libobs.obs_audio_info avi = new libobs.obs_audio_info
+            {
+                samples_per_sec = Constants.Audio.SAMPLES_PER_SEC,
+                speakers = libobs.speaker_layout.SPEAKERS_STEREO
+            };
+
+            if (!Obs.ResetAudio(avi))
+                throw new ApplicationException("ResetAudio failed.");
+        }
+
+        //private void ResetVideoInfo()
+        //{
+        //    if (presentation != null)
+        //    {
+        //        if (presentation.SelectedScene.GetName().ToLowerInvariant() != "main")
+        //        {
+        //            presentation.SetScene(0);
+        //        }
+        //    }
+
+        //    // When recording in fullscreen, never switch screens. At least until we have a good way to scale videos with different resolutions when concatenating
+        //    Screen screenToRecord = screenForFullscreenRecording ?? Util.GetActiveScreen();
+        //    Rect activeScreenBounds = DpiUtil.GetScreenWpfBounds(screenToRecord);
+        //    _physicalRecordingScreen = screenToRecord;
+
+        //    // Fullscreen
+        //    if (MainWindowAccessor.Window.selectionWindow == null)
+        //    {
+        //        System.Windows.Point obsAdj = DpiUtil.GetWpfSizeAdjustmentForObs(activeScreenBounds, activeScreenBounds.Width, activeScreenBounds.Height, true);
+        //        Rect adjustedActiveScreenBounds = activeScreenBounds;
+        //        adjustedActiveScreenBounds.Width += obsAdj.X;
+        //        adjustedActiveScreenBounds.Height += obsAdj.Y;
+
+        //        int originalCanvasWidth = DpiUtil.ConvertSizeWpfToPhysicalPixel(activeScreenBounds.Width);
+        //        int originalCanvasHeight = DpiUtil.ConvertSizeWpfToPhysicalPixel(activeScreenBounds.Height);
+        //        canvasWidth = DpiUtil.ConvertSizeWpfToPhysicalPixel(adjustedActiveScreenBounds.Width);
+        //        canvasHeight = DpiUtil.ConvertSizeWpfToPhysicalPixel(adjustedActiveScreenBounds.Height);
+
+        //        int recordingWidthAdj = originalCanvasWidth - canvasWidth;
+        //        int recordingHeightAdj = originalCanvasHeight - canvasHeight;
+        //        int cropLeft = recordingWidthAdj / 2;
+        //        int cropTop = recordingHeightAdj / 2;
+        //        int cropRight = recordingWidthAdj - cropLeft;
+        //        int cropBottom = recordingHeightAdj - cropTop;
+
+        //        AppliedCrop = new libobs.obs_sceneitem_crop
+        //        {
+        //            left = cropLeft,
+        //            top = cropTop,
+        //            right = cropRight,
+        //            bottom = cropBottom
+        //        };
+        //    }
+        //    // Selection window
+        //    else
+        //    {
+        //        // We need to get these values from the main thread and save them got references
+        //        Util.WindowSizeProperties selectionWindowSize = GetWindowSizeFromMainThread(MainWindowAccessor.Window.selectionWindow);
+        //        canvasWidth = DpiUtil.ConvertSizeWpfToPhysicalPixel(selectionWindowSize.Width - (selectionWindowSize.BorderWidth.Value * 2));
+        //        canvasHeight = DpiUtil.ConvertSizeWpfToPhysicalPixel(selectionWindowSize.Height - (selectionWindowSize.BorderWidth.Value * 2));
+
+        //        int cropLeft = DpiUtil.ConvertSizeWpfToPhysicalPixel(selectionWindowSize.Left - activeScreenBounds.X + selectionWindowSize.BorderWidth.Value);
+        //        int cropTop = DpiUtil.ConvertSizeWpfToPhysicalPixel(selectionWindowSize.Top - activeScreenBounds.Y + selectionWindowSize.BorderWidth.Value);
+        //        int cropRight = DpiUtil.ConvertSizeWpfToPhysicalPixel(activeScreenBounds.Width) - cropLeft - canvasWidth;
+        //        int cropBottom = DpiUtil.ConvertSizeWpfToPhysicalPixel(activeScreenBounds.Height) - cropTop - canvasHeight;
+
+        //        AppliedCrop = new libobs.obs_sceneitem_crop
+        //        {
+        //            left = cropLeft,
+        //            top = cropTop,
+        //            right = cropRight,
+        //            bottom = cropBottom,
+        //        };
+        //    }
+
+        //    // Set the proper display source
+        //    if (source_display != null)
+        //    {
+        //        ObsData displaySettings = new ObsData();
+        //        displaySettings.SetBool("capture_cursor", true);
+        //        displaySettings.SetInt("monitor", GetObsDisplayValueFromScreen(screenToRecord));
+        //        source_display.Update(displaySettings);
+        //        displaySettings.Dispose();
+        //    }
+
+        //    // Set the proper display bounds and crop
+        //    if (item_display != null)
+        //    {
+        //        item_display.SetBounds(new Vector2(0, 0), ObsBoundsType.None, ObsAlignment.Top);
+        //        item_display.SetCrop(AppliedCrop);
+        //    }
+
+        //    CalculateWebcamItemPosition();
+
+        //    libobs.obs_video_info ovi = GenerateObsVideoInfoObject(
+        //        (uint)canvasWidth,
+        //        (uint)canvasHeight,
+        //        (uint)DpiUtil.ConvertSizeRectDpiToSystemDpi(activeScreenBounds, canvasWidth),
+        //        (uint)DpiUtil.ConvertSizeRectDpiToSystemDpi(activeScreenBounds, canvasHeight));
+
+        //    if (!Obs.ResetVideo(ovi))
+        //        throw new ApplicationException("ResetVideo failed.");
+        //}
     }
 }
---
+++ Utility/Constants.cs
@@ -0,0 +1,79 @@
+﻿namespace obs_cli.Utility
+{
+	class Constants
+	{
+		public static class Video
+		{
+			public const int FPS = 15;
+			public const int FPS_DEN = 1;
+			public const int ENCODER_BITRATE = 2500;
+			public const string RATE_CONTROL = "VBR";
+		}
+
+		public static class Audio
+		{
+			public const int SAMPLES_PER_SEC = 44100;
+			public const int ENCODER_BITRATE = 160;
+			public const string RATE_CONTROL = "VBR";
+
+			public const string NO_DEVICE_ID = "none";
+			public const string DEFAULT_DEVICE_ID = "default";
+
+			/* Custom audio delays in nanoseconds */
+			public const int NANOSEC_PER_MSEC = 1000000;
+			public const int DELAY_OUTPUT = 67 * NANOSEC_PER_MSEC;
+			public const int DELAY_INPUT = 137 * NANOSEC_PER_MSEC;
+			public const int DELAY_INPUT_NOT_ATTACHED_TO_WEBCAM = 234 * NANOSEC_PER_MSEC;
+			public const int DELAY_INPUT_ATTACHED_TO_WEBCAM = 200 * NANOSEC_PER_MSEC;
+		}
+
+		public static class RegexPatterns
+		{
+			public const string Guid =
+				@"(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}";
+		}
+
+		public static class RegistryKeys
+		{
+			public static string CLASSES = "Classes";
+			public static string DEFAULT_ICON = "DefaultIcon";
+			public static string ILOS_CO = "ilosco";
+			//public static string ILOS_RECORD = Settings.Default.IlosUrlProtocol;
+			public static string INSTALLER_PATH_KEY = "InstallerPath";
+			public static string PROXY_HOST = "proxy_host";
+			public static string PROXY_PORT = "proxy_port";
+			public static string PROXY_SET = "proxy_set";
+			public static string RECORDER = "recorder";
+			public static string SOFTWARE = "SOFTWARE";
+			public static string URL_PROTOCOL = "URL Protocol";
+			public static string VIDGRID = "VidGrid";
+		}
+
+		// See https://wiki.winehq.org/List_Of_Windows_Messages for more codes.
+		internal enum WindowMessage
+		{
+			WM_DPICHANGED = 0x02E0,
+
+			WM_ENTERSIZEMOVE = 0x0231,
+			WM_EXITSIZEMOVE = 0x0232,
+
+			WM_MOVE = 0x0003,
+			WM_SIZE = 0x0005,
+
+			WM_ACTIVATE = 0x0006,
+
+			WM_DWMCOLORIZATIONCOLORCHANGED = 0x0320,
+
+			WM_NCLBUTTONDOWN = 0xA1,
+			HTCAPTION = 0x2,
+
+			// mouse events
+			WM_LBUTTONDOWN = 0x0201,
+			WM_LBUTTONUP = 0x0202,
+			WM_MOUSEMOVE = 0x0200,
+			WM_MOUSEWHEEL = 0x020A,
+			WM_RBUTTONDOWN = 0x0204,
+			WM_RBUTTONUP = 0x0205
+		}
+	}
+}
