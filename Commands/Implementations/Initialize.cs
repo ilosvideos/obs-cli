d46b4fb46d3da3f6cae4c785a1b73faef515be3f
Chad Luangrath <chad.luangrath@ilosvideos.com>
2020-03-30 17:05:24 -0500

    accept more parameters in initialization

--- Commands/Implementations/Initialize.cs
+++ Commands/Implementations/Initialize.cs
@@ -1,17 +1,31 @@
 ﻿using OBS;
 using obs_cli.Helpers;
+using obs_cli.Objects;
 using obs_cli.Utility;
 using System;
 using System.Collections.Generic;
+using static OBS.libobs;
 
 namespace obs_cli.Commands.Implementations
 {
+    // todo: need to do InitPresentation logic still
     public class Initialize : ICommand
     {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public int Height { get; set; }
-        public int Width { get; set; }
+        public int CropTop { get; set; }
+        public int CropRight { get; set; }
+        public int CropLeft { get; set; }
+        public int CropBottom { get; set; }
+        public uint FrameRate { get; set; }
+        public double OutputWidth { get; set; }
+        public double OutputHeight { get; set; }
+        public int CanvasWidth { get; set; }
+        public int CanvasHeight { get; set; }
+
+        public Presentation Presentation;
+        public obs_sceneitem_crop AppliedCrop;
+
+        public Source DisplaySource;
+        public Item DisplayItem;
 
         public static string Name
         {
@@ -25,15 +39,27 @@ namespace obs_cli.Commands.Implementations
         {
             FileWriteService.WriteToFile($"got {arguments.Count} arguments");
 
-            this.X = int.Parse(arguments["x"]);
-            this.Y = int.Parse(arguments["y"]);
-            this.Width = int.Parse(arguments["width"]);
-            this.Height = int.Parse(arguments["height"]);
+            this.CropTop = int.Parse(arguments["cropTop"]);
+            this.CropRight = int.Parse(arguments["cropRight"]);
+            this.CropBottom = int.Parse(arguments["cropBottom"]);
+            this.CropLeft = int.Parse(arguments["cropLeft"]);
+            this.FrameRate = uint.Parse(arguments["frameRate"]);
+            this.CanvasWidth = int.Parse(arguments["canvasWidth"]);
+            this.CanvasHeight = int.Parse(arguments["canvasHeight"]);
+            this.OutputWidth = double.Parse(arguments["outputWidth"]);
+            this.OutputHeight = double.Parse(arguments["outputHeight"]);
 
-            FileWriteService.WriteToFile($"Initializing with X position: {X}");
-            FileWriteService.WriteToFile($"Initializing with Y position: {Y}");
-            FileWriteService.WriteToFile($"Initializing with Width: {Width}");
-            FileWriteService.WriteToFile($"Initializing with Height position: {Height}");
+            FileWriteService.WriteToFile($"Initializing with CropTop: {CropTop}");
+            FileWriteService.WriteToFile($"Initializing with CropRight: {CropRight}");
+            FileWriteService.WriteToFile($"Initializing with CropBottom: {CropBottom}");
+            FileWriteService.WriteToFile($"Initializing with CropLeft: {CropLeft}");
+
+            FileWriteService.WriteToFile($"Initializing with FrameRate: {FrameRate}");
+
+            FileWriteService.WriteToFile($"Initializing with CanvasWidth: {CanvasWidth}");
+            FileWriteService.WriteToFile($"Initializing with CanvasHeight: {CanvasHeight}");
+            FileWriteService.WriteToFile($"Initializing with OutputWidth: {OutputWidth}");
+            FileWriteService.WriteToFile($"Initializing with OutputHeight: {OutputHeight}");
         }
 
         public void Execute()
@@ -50,7 +76,9 @@ namespace obs_cli.Commands.Implementations
 
             FileWriteService.WriteToFile("ResetAudioInfo successful");
 
-            // todo: ResetVideoInfo();
+            ResetVideoInfo();
+
+            FileWriteService.WriteToFile("ResetVideoInfo successful");
 
             Obs.LoadAllModules();
 
@@ -71,98 +99,117 @@ namespace obs_cli.Commands.Implementations
                 throw new ApplicationException("ResetAudio failed.");
         }
 
-        //private void ResetVideoInfo()
+        private uint GetFrameRate()
+        {
+            int minfps = 4;
+            int maxfps = 30;
+            int fps = (int)FrameRate;
+
+            if (fps < 4)
+            {
+                fps = minfps;
+            }
+            else if (fps > maxfps)
+            {
+                fps = maxfps;
+            }
+
+            return (uint)fps;
+        }
+
+        private void ResetVideoInfo()
+        {
+            if (Presentation != null)
+            {
+                if (Presentation.SelectedScene.GetName().ToLowerInvariant() != "main")
+                {
+                    Presentation.SetScene(0);
+                }
+            }
+
+            AppliedCrop = new libobs.obs_sceneitem_crop
+            {
+                left = CropLeft,
+                top = CropTop,
+                right = CropRight,
+                bottom = CropBottom
+            };
+
+            //Set the proper display source
+            if (DisplaySource != null)
+            {
+                ObsData displaySettings = new ObsData();
+                displaySettings.SetBool("capture_cursor", true);
+                //displaySettings.SetInt("monitor", GetObsDisplayValueFromScreen(screenToRecord));
+                DisplaySource.Update(displaySettings);
+                displaySettings.Dispose();
+            }
+
+            //Set the proper display bounds and crop
+            if (DisplayItem != null)
+            {
+                DisplayItem.SetBounds(new Vector2(0, 0), ObsBoundsType.None, ObsAlignment.Top);
+                DisplayItem.SetCrop(AppliedCrop);
+            }
+
+            //CalculateWebcamItemPosition();
+
+            libobs.obs_video_info ovi = GenerateObsVideoInfoObject(
+                (uint)CanvasWidth,
+                (uint)CanvasHeight,
+                (uint)OutputWidth,
+                (uint)OutputHeight);
+
+            if (!Obs.ResetVideo(ovi))
+                throw new ApplicationException("ResetVideo failed.");
+        }
+
+        /** 
+		 * Get the active display from the window's position.
+		 * We have to do it this way because the OBS index (or display value) is different than that of Screen.AllScreens
+		 */
+        //private int GetObsDisplayValueFromScreen(Screen screen)
         //{
-        //    if (presentation != null)
+        //    // Get a list of OBS properties (names and values) from the display source
+        //    ObsProperty[] displayCaptureProperties = source_display.GetProperties().GetPropertyList();
+        //    List<string> displayNames = new List<string>();
+        //    List<object> displayValues = new List<object>();
+        //    for (int i = 0; i < displayCaptureProperties.Length; i++)
         //    {
-        //        if (presentation.SelectedScene.GetName().ToLowerInvariant() != "main")
+        //        if (displayCaptureProperties[i].Name.Equals("monitor"))
         //        {
-        //            presentation.SetScene(0);
+        //            displayNames = displayCaptureProperties[i].GetListItemNames().ToList();
+        //            displayValues = displayCaptureProperties[i].GetListItemValues().ToList();
+        //            break;
         //        }
         //    }
 
-        //    // When recording in fullscreen, never switch screens. At least until we have a good way to scale videos with different resolutions when concatenating
-        //    Screen screenToRecord = screenForFullscreenRecording ?? Util.GetActiveScreen();
-        //    Rect activeScreenBounds = DpiUtil.GetScreenWpfBounds(screenToRecord);
-        //    _physicalRecordingScreen = screenToRecord;
+        //    // Find the OBS display that matches the bounds of our active screen. OBS display names are in the format of "Display {value}: WidthxHeight @ X,Y"
+        //    string searchForString = $"@ {screen.Bounds.X},{screen.Bounds.Y}";
+        //    int targetDisplayIndex = displayNames.FindIndex(x => x.Contains(searchForString));
+        //    int targetDisplayValue = int.Parse(displayValues[targetDisplayIndex].ToString());
 
-        //    // Fullscreen
-        //    if (MainWindowAccessor.Window.selectionWindow == null)
-        //    {
-        //        System.Windows.Point obsAdj = DpiUtil.GetWpfSizeAdjustmentForObs(activeScreenBounds, activeScreenBounds.Width, activeScreenBounds.Height, true);
-        //        Rect adjustedActiveScreenBounds = activeScreenBounds;
-        //        adjustedActiveScreenBounds.Width += obsAdj.X;
-        //        adjustedActiveScreenBounds.Height += obsAdj.Y;
-
-        //        int originalCanvasWidth = DpiUtil.ConvertSizeWpfToPhysicalPixel(activeScreenBounds.Width);
-        //        int originalCanvasHeight = DpiUtil.ConvertSizeWpfToPhysicalPixel(activeScreenBounds.Height);
-        //        canvasWidth = DpiUtil.ConvertSizeWpfToPhysicalPixel(adjustedActiveScreenBounds.Width);
-        //        canvasHeight = DpiUtil.ConvertSizeWpfToPhysicalPixel(adjustedActiveScreenBounds.Height);
-
-        //        int recordingWidthAdj = originalCanvasWidth - canvasWidth;
-        //        int recordingHeightAdj = originalCanvasHeight - canvasHeight;
-        //        int cropLeft = recordingWidthAdj / 2;
-        //        int cropTop = recordingHeightAdj / 2;
-        //        int cropRight = recordingWidthAdj - cropLeft;
-        //        int cropBottom = recordingHeightAdj - cropTop;
-
-        //        AppliedCrop = new libobs.obs_sceneitem_crop
-        //        {
-        //            left = cropLeft,
-        //            top = cropTop,
-        //            right = cropRight,
-        //            bottom = cropBottom
-        //        };
-        //    }
-        //    // Selection window
-        //    else
-        //    {
-        //        // We need to get these values from the main thread and save them got references
-        //        Util.WindowSizeProperties selectionWindowSize = GetWindowSizeFromMainThread(MainWindowAccessor.Window.selectionWindow);
-        //        canvasWidth = DpiUtil.ConvertSizeWpfToPhysicalPixel(selectionWindowSize.Width - (selectionWindowSize.BorderWidth.Value * 2));
-        //        canvasHeight = DpiUtil.ConvertSizeWpfToPhysicalPixel(selectionWindowSize.Height - (selectionWindowSize.BorderWidth.Value * 2));
-
-        //        int cropLeft = DpiUtil.ConvertSizeWpfToPhysicalPixel(selectionWindowSize.Left - activeScreenBounds.X + selectionWindowSize.BorderWidth.Value);
-        //        int cropTop = DpiUtil.ConvertSizeWpfToPhysicalPixel(selectionWindowSize.Top - activeScreenBounds.Y + selectionWindowSize.BorderWidth.Value);
-        //        int cropRight = DpiUtil.ConvertSizeWpfToPhysicalPixel(activeScreenBounds.Width) - cropLeft - canvasWidth;
-        //        int cropBottom = DpiUtil.ConvertSizeWpfToPhysicalPixel(activeScreenBounds.Height) - cropTop - canvasHeight;
-
-        //        AppliedCrop = new libobs.obs_sceneitem_crop
-        //        {
-        //            left = cropLeft,
-        //            top = cropTop,
-        //            right = cropRight,
-        //            bottom = cropBottom,
-        //        };
-        //    }
-
-        //    // Set the proper display source
-        //    if (source_display != null)
-        //    {
-        //        ObsData displaySettings = new ObsData();
-        //        displaySettings.SetBool("capture_cursor", true);
-        //        displaySettings.SetInt("monitor", GetObsDisplayValueFromScreen(screenToRecord));
-        //        source_display.Update(displaySettings);
-        //        displaySettings.Dispose();
-        //    }
-
-        //    // Set the proper display bounds and crop
-        //    if (item_display != null)
-        //    {
-        //        item_display.SetBounds(new Vector2(0, 0), ObsBoundsType.None, ObsAlignment.Top);
-        //        item_display.SetCrop(AppliedCrop);
-        //    }
-
-        //    CalculateWebcamItemPosition();
-
-        //    libobs.obs_video_info ovi = GenerateObsVideoInfoObject(
-        //        (uint)canvasWidth,
-        //        (uint)canvasHeight,
-        //        (uint)DpiUtil.ConvertSizeRectDpiToSystemDpi(activeScreenBounds, canvasWidth),
-        //        (uint)DpiUtil.ConvertSizeRectDpiToSystemDpi(activeScreenBounds, canvasHeight));
-
-        //    if (!Obs.ResetVideo(ovi))
-        //        throw new ApplicationException("ResetVideo failed.");
+        //    return targetDisplayValue;
         //}
+
+        private libobs.obs_video_info GenerateObsVideoInfoObject(uint baseWidth, uint baseHeight, uint outputWidth, uint outputHeight)
+        {
+            return new libobs.obs_video_info
+            {
+                adapter = 0,
+                base_width = baseWidth,
+                base_height = baseHeight,
+                output_width = outputWidth,
+                output_height = outputHeight,
+                fps_num = GetFrameRate(),
+                fps_den = Constants.Video.FPS_DEN,
+                graphics_module = "libobs-d3d11.dll",
+                output_format = libobs.video_format.VIDEO_FORMAT_NV12,
+                scale_type = libobs.obs_scale_type.OBS_SCALE_BICUBIC,
+                colorspace = libobs.video_colorspace.VIDEO_CS_601,
+                range = libobs.video_range_type.VIDEO_RANGE_PARTIAL,
+                gpu_conversion = true,
+            };
+        }
     }
 }
