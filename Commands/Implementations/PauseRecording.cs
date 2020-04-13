34e0fb8657acf82d56c1e59ded252a7f5e1fce10
Chad Luangrath <chad.luangrath@ilosvideos.com>
2020-04-13 14:08:38 -0500

    wait until Obs output has stopped to dispose encoders

--- Commands/Implementations/PauseRecording.cs
+++ Commands/Implementations/PauseRecording.cs
@@ -1,22 +1,45 @@
 ﻿using obs_cli.Data;
 using obs_cli.Helpers;
 using System.Collections.Generic;
+using System.Timers;
 
 namespace obs_cli.Commands.Implementations
 {
     public class PauseRecording : BaseCommand
     {
+        public Timer OutputPauseTimer { get; set; }
+
         public override string Name => AvailableCommand.PauseRecording.GetDescription();
 
         public PauseRecording(IDictionary<string, string> arguments)
         {
-            
+            OutputPauseTimer = new Timer();
         }
 
         public override void Execute()
         {
             FileWriteService.WriteLineToFile("start pause recording");
             Store.Data.Record.OutputAndEncoders.obsOutput.Stop();
+
+            OutputPauseTimer = new Timer();
+            OutputPauseTimer.Interval = 50;
+            OutputPauseTimer.Elapsed += new ElapsedEventHandler(PauseRecordingWhenOutputInactive);
+            OutputPauseTimer.Enabled = true;
+            OutputPauseTimer.Start();
+        }
+
+        private void PauseRecordingWhenOutputInactive(object source, ElapsedEventArgs e)
+        {
+            if (Store.Data.Record.OutputAndEncoders.obsOutput != null && Store.Data.Record.OutputAndEncoders.obsOutput.Active)
+            {
+                return;
+            }
+
+            OutputPauseTimer.Stop();
+            OutputPauseTimer.Dispose();
+            OutputPauseTimer = null;
+
+            Store.Data.Record.OutputAndEncoders.Dispose();
         }
     }
 }
