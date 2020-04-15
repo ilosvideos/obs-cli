aee1c75c7656e744061f5a2f474155daaa9b4ffa
Chad Luangrath <chad.luangrath@ilosvideos.com>
2020-04-15 17:44:27 -0500

    move audio device list to separate class file

--- Commands/Implementations/GetAudioDevices.cs
+++ Commands/Implementations/GetAudioDevices.cs
@@ -1,32 +1,10 @@
 ﻿using obs_cli.Enums;
+using obs_cli.Objects;
 using obs_cli.Services;
-using System;
 using System.Collections.Generic;
-using System.Web.Script.Serialization;
 
 namespace obs_cli.Commands.Implementations
 {
-    public class AudioDevice
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-    }
-
-    public class AudioDeviceList
-    {
-        public List<AudioDevice> Devices { get; set; }
-
-        public AudioDeviceList()
-        {
-            Devices = new List<AudioDevice>();
-        }
-
-        public void Add(AudioDevice device)
-        {
-            this.Devices.Add(device);
-        }
-    }
-
     public class GetAudioDevices : BaseCommand
     {
         public override string Name => AvailableCommand.GetAudioDevices.GetDescription();
@@ -41,7 +19,7 @@ namespace obs_cli.Commands.Implementations
             var obsAudioDevices = AudioService.GetAudioInputDevices();
 
             var audioDeviceList = new AudioDeviceList();
-            obsAudioDevices.ForEach(x => audioDeviceList.Add(new AudioDevice() { Id = x.id, Name = x.name }));
+            obsAudioDevices.ForEach(x => audioDeviceList.Add(x));
 
             EmitService.EmitAudioDevices(audioDeviceList);
         }
---
+++ Objects/AudioDeviceList.cs
@@ -0,0 +1,20 @@
+﻿using obs_cli.Structs;
+using System.Collections.Generic;
+
+namespace obs_cli.Objects
+{
+    public class AudioDeviceList
+    {
+        public List<AudioDevice> Devices { get; set; }
+
+        public AudioDeviceList()
+        {
+            Devices = new List<AudioDevice>();
+        }
+
+        public void Add(AudioDevice device)
+        {
+            this.Devices.Add(device);
+        }
+    }
+}
