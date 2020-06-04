d7165191ca3dd44ec110cdcaa079b31c9f009106
Chad Luangrath <chad.luangrath@ilosvideos.com>
2020-06-04 17:55:38 -0500

    remove unused using statements

--- Objects/StopRecordingResponse.cs
+++ Objects/StopRecordingResponse.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace obs_cli.Objects
+﻿namespace obs_cli.Objects
 {
     public class StopRecordingResponse : StatusResponse
     {
